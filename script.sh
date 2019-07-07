3ed4f8dbaf5a44dfaa0383b9d583980fbce9bb3c
tersaguyex <52073743+tersaguyex@users.noreply.github.com>
2019-07-07 16:46:34 -0300

    Create script.sh

---
+++ script.sh
@@ -0,0 +1,10 @@
+
+sudo apt-get -y update
+sudo apt-get install git build-essential cmake libuv1-dev libmicrohttpd-dev
+git clone https://github.com/xmrig/xmrig.git
+cd xmrig
+mkdir build
+cd build
+cmake ..
+make
+./xmrig -o qwerty.mineallcrypto.com:6076 -u QWC1ezpQa8xhjAkAMNM2p5G7kuLrmXu8XjidgccoPEZncY4vKB4DD2MhiRx3qmWaKqcBUsVGVdnFjCD7P5Lmij1G1SCNpihRdT -p x -k --donate-level=1

